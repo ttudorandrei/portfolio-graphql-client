944dd8d1f15dda398561c15ddfaa4724cdae42a1
Tudor Tocan <tudor.tocan@gmail.com>
2021-07-16 16:00:19 +0100

    add repo cards, they render with static data

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,7 +2,8 @@ import RepoCard from "../RepoCard";
 
 const Portfolio = ({ repos }) => {
   return (
-    <div>
+    <div className="container d-flex flex-column  justify-content-evenly mt-5">
+      <div>My Projects</div>
       {repos.map((repo) => {
         return <RepoCard repo={repo} />;
       })}
--- src/components/RepoCard/index.js
+++ src/components/RepoCard/index.js
@@ -1,19 +1,17 @@
 const RepoCard = ({ repo }) => {
   return (
-    <div>
-      <div className="card mb-3">
-        <a href={repo.repoUrl}>
-          <img src="..." className="card-img-top" alt="..." />
-        </a>
-        <div className="card-body">
-          <h5 className="card-title">{repo.repoName}</h5>
-          <div className="card-text"> Created at {repo.createdAt}</div>
-          <div className="card-text">Description (not from api)</div>
-          <div className="card-text">
-            <small className="text-muted">
-              Technologies used: {repo.repoLanguage}
-            </small>
-          </div>
+    <div className="card mb-3 w-50">
+      <a href={repo.repoUrl}>
+        <img src="..." className="card-img-top" alt="..." />
+      </a>
+      <div className="card-body">
+        <h5 className="card-title">{repo.repoName}</h5>
+        <div className="card-text"> Created at {repo.createdAt}</div>
+        <div className="card-text">Description (not from api)</div>
+        <div className="card-text">
+          <small className="text-muted">
+            Technologies used: {repo.repoLanguage}
+          </small>
         </div>
       </div>
     </div>
