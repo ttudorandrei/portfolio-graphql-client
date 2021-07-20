fe8dc229b953a59463eed1e5e990ca836f227923
Tudor Tocan <tudor.tocan@gmail.com>
2021-07-20 14:36:12 +0100

    refactor (WIP)

--- src/pages/ContactMe/ContactMe.js
+++ src/pages/ContactMe/ContactMe.js
@@ -5,52 +5,42 @@ const ContactMe = ({ user }) => {
     <Fade>
       <div className="container d-flex flex-column align-items-center mt-5 neg-zi">
         <h3>Contact</h3>
+        <div className="container d-flex flex-row justify-content-evenly mt-3">
+          {/* form should go here */}
+        </div>
+        <h4>Or you can stalk me on social media</h4>
         <div className="container d-flex flex-column flex-md-row justify-content-evenly mt-3">
           {/* LinkedIn Logo */}
-          <div className="text-center mt-3">
-            <a
-              href="https://linkedin.com/in/tudor-tocan"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img src="/assets/img/linkedin.png" alt="linkedin logo" />
-            </a>
-            <div>LinkedIn</div>
-          </div>
+          <a
+            href="https://linkedin.com/in/tudor-tocan"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <div className="text-center mt-3">
+              <img src="../assets/img/linkedin.png" alt="linkedin logo" />
+              <div>LinkedIn</div>
+            </div>
+          </a>
 
           {/* GitHub Logo */}
-          <div className="text-center mt-3">
-            <a
-              href="https://github.com/ttudorandrei"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img src="/assets/img/github-logo-dark.png" alt="github logo" />
-            </a>
-            <div>GitHub</div>
-          </div>
+          <a
+            href="https://github.com/ttudorandrei"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <div className="text-center mt-3">
+              <img src="../assets/img/github-logo-dark.png" alt="github logo" />
+              <div>GitHub</div>
+            </div>
+          </a>
 
           {/* Gmail Logo */}
-          <div className="text-center mt-3">
-            <a href="mailto:tudor.tocan@gmail.com">
-              <img src="assets/img/gmail.png" alt="gmail logo" />
-            </a>
-            <div>GMail</div>
-          </div>
-
-          {/* Download Resume */}
-          <div className="text-center mt-3">
-            <a
-              href="/assets/doc/tudor-andrei-tocan-resume.pdf"
-              download="Tudor Andrei Tocan - Resume"
-            >
-              <img
-                src="assets/img/resume-icon.png"
-                alt="download y resume"
-              ></img>
-            </a>
-            <div>Download My Resume</div>
-          </div>
+          <a href="mailto:tudor.tocan@gmail.com">
+            <div className="text-center mt-3">
+              <img src="../assets/img/gmail.png" alt="gmail logo" />
+              <div>GMail</div>
+            </div>
+          </a>
         </div>
       </div>
     </Fade>
