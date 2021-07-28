9f8ee9e3445bb09953abb2f8b71e2fd1c6b140dc
Tudor Tocan <tudor.tocan@gmail.com>
2021-07-28 16:46:28 +0100

    styled repo card

--- src/components/RepoCard/RepoCard.js
+++ src/components/RepoCard/RepoCard.js
@@ -6,24 +6,23 @@ import formatRepoName from "../../utils/helpers/formatRepoName";
 const RepoCard = ({ repo }) => {
   return (
     <Fade>
-      <div className="d-flex flex-wrap flex-lg-row flex-md-column flex-sm-column justify-content-evenly align-items-center text-center mb-5 project-card dark-font p-3 m-3 w-100 h-auto">
+      <div className="d-flex flex-wrap flex-lg-row flex-md-column flex-sm-column justify-content-evenly align-items-center text-center mb-5 project-card p-3 m-3 w-75 dark-font h-auto repo-card">
         <div className="">
           <a href={repo.repoUrl}>
             <img
               src={`assets/img/${repo.repoName}.png`}
               className="card-img-top"
               alt="..."
-              style={{ width: "40%", height: "auto" }}
+              style={{ width: "50%", height: "auto" }}
             />
           </a>
         </div>
 
         <div
           className="mt-3 text-center"
-          style={{ width: "400px", height: "auto" }}
+          style={{ width: "50%", height: "auto" }}
         >
           <h5 className="card-title">{formatRepoName(repo.repoName)}</h5>
-          {/* <h5 className="card-title">{repo.repoName}</h5> */}
 
           <div className="card-text">
             Created at{" "}
@@ -31,7 +30,7 @@ const RepoCard = ({ repo }) => {
           </div>
           <div className="card-text">{repo.repoDescription}</div>
           <div className="card-text">
-            Check it out{" "}
+            Check out the deployed application{" "}
             <a href={repo.deployedUrl} className="highlight">
               here
             </a>
